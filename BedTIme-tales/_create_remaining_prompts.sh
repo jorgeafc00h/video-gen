93f06b806278bcc23e677a35986dce217699b9e0
jorgeafc00h <jorgeafc00h@gmail.com>
2026-02-19 14:30:40 -0600

    Fix Git index and stage all changes

--- BedTIme-tales/_create_remaining_prompts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This script will be used to batch create the remaining optimized prompts
-# Gatita (anime), Cierva (watercolor), Zorro (watercolor)
-echo "Ready to create remaining LTX2-optimized prompts"
